0c3ae75f1a5c9d68e0fad12cbede94dbfb0d4854
Sam Nelson <samn10.nelson@gmail.com>
2024-12-31 16:16:33 -0800

    added day 2 code

---
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/convert_ages_to_days.d
@@ -0,0 +1,1 @@
+/Users/sam/repos/rust100days/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/convert_ages_to_days: /Users/sam/repos/rust100days/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/src/main.rs
---
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-21b53a6bffcfbc10.d
@@ -0,0 +1,5 @@
+/Users/sam/repos/rust100days/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-21b53a6bffcfbc10: src/main.rs
+
+/Users/sam/repos/rust100days/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-21b53a6bffcfbc10.d: src/main.rs
+
+src/main.rs:
